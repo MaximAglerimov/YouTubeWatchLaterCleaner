7357d329ae04551f1127d64317895faeadbdabf4
Maxim Aglerimov <maxaglerimov@gmail.com>
2025-11-17 22:46:29 +0400

    fix permission errors during video deletion

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export async function main() {
       const deleteButton = menu?.getDeleteButton();
       deleteButton?.click();
 
-      await delayMs(100);
+      await delayMs(1200);
     }
   } catch (error) {
     console.error("Error in main:", error);
