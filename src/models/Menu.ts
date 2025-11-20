f044e5b808eb80755a151e4351999c0cbea405f7
Maxim Aglerimov <maxaglerimov@gmail.com>
2025-11-20 23:09:38 +0400

    get delete button by nth-child

--- src/models/Menu.ts
+++ src/models/Menu.ts
@@ -9,15 +9,29 @@ export class DomMenu implements IMenu {
   constructor(public element: Element) {}
 
   getDeleteButton(): IButton | null {
-    const allElements = this.element.querySelectorAll("tp-yt-paper-item");
+    let buttonElement = this.getByText(this.element);
+    buttonElement ??= this.getByIcon(this.element);
 
-    const buttonElement = Array.from(allElements).filter(
-      (el) => (el as HTMLElement).innerText === "Remove from Watch later"
-    )[0];
     if (buttonElement == null) {
       return null;
     }
 
     return new DomButton(buttonElement);
+  }
+
+  private getByText(menuElement: Element): Element | null {
+    const allElements = menuElement.querySelectorAll("tp-yt-paper-item");
+
+    return Array.from(allElements).filter(
+      (el) => (el as HTMLElement).innerText === "Remove from Watch later"
+    )[0];
+  }
+
+  private getByIcon(menuElement: Element): Element | null {
+    const allElements = menuElement.querySelectorAll(
+      "tp-yt-paper-listbox#items > ytd-menu-service-item-renderer:nth-child(3)"
+    );
+
+    return allElements[0];
   }
 }
